0c7fb168f62629e931af917e706466f5d509a15e
Spencer Farley <2847259+farlee2121@users.noreply.github.com>
2023-09-22 12:30:42 -0500

    Fix missing blazor js file

--- src/Elmish.HotReload.Client.Bolero/Startup.fs
+++ src/Elmish.HotReload.Client.Bolero/Startup.fs
@@ -5,6 +5,7 @@ open Microsoft.AspNetCore.Blazor.Hosting
 open Microsoft.AspNetCore.Components.Builder
 open Microsoft.Extensions.DependencyInjection
 open Microsoft.Extensions.Logging
+open Microsoft.AspNetCore.Components.WebAssembly.Hosting
 
 type Startup() =
 
@@ -17,7 +18,7 @@ type Startup() =
 
 
     member __.Configure(app: IComponentsApplicationBuilder) =
-        app.AddComponent<Main.MyApp>("#main")
+       app.AddComponent<Main.MyApp>("#main")
 
 
 
@@ -25,9 +26,15 @@ module Program =
 
     [<EntryPoint>]
     let Main args =
+        // let builder = WebAssemblyHostBuilder.CreateDefault(args)
+        // builder.RootComponents.Add<Main.MyApp>("#main")
+        // //builder.Services.AddBoleroRemoting(builder.HostEnvironment) |> ignore
+        // builder.Build().RunAsync() |> ignore
+        // 0
+        
         BlazorWebAssemblyHost
-            .CreateDefaultBuilder()
-            .UseBlazorStartup<Startup>()
-            .Build()
-            .Run()
+           .CreateDefaultBuilder()
+           .UseBlazorStartup<Startup>()
+           .Build()
+           .Run()
         0
